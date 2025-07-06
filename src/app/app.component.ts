419129dad7fd320a6f5d69f5662c33a4dfb21b17
raj kumar <raaz.kumar015@gmail.com>
2025-07-06 22:44:47 +0530

    starting topic by branch -- Template Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { UserProfileComponent } from "./user-profile/user-profile.component";
 import { AnimalService } from './animal.service';
 import { ChildComponent } from "./child/child.component";
 import { TestcomComponent } from "./signaldoc/testcom/testcom.component";
+import { TemplateExampleComponent } from './template-example/template-example/template-example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfileComponent, ChildComponent, TestcomComponent],
+  imports: [RouterOutlet, UserProfileComponent, ChildComponent, TestcomComponent,TemplateExampleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   viewProviders: [{provide:AnimalService, useClass: AnimalService}],
---
+++ src/app/template-example/template-example/template-example.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-example',
+  imports: [],
+  templateUrl: './template-example.component.html',
+  styleUrl: './template-example.component.css'
+})
+export class TemplateExampleComponent {
+
+  isFormValid: boolean = true;
+}
