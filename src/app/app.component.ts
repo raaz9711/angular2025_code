39bdc3e9e2442724df157d2d1fd87414f19829da
raj kumar <raaz.kumar015@gmail.com>
2025-07-08 09:12:47 +0530

    Adding some more changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { UserProfileComponent } from "./user-profile/user-profile.component";
 import { AnimalService } from './animal.service';
 import { ChildComponent } from "./child/child.component";
 import { TestcomComponent } from "./signaldoc/testcom/testcom.component";
+import { ProjectionComponent } from "./content-projection/projection/projection.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfileComponent, ChildComponent, TestcomComponent],
+  imports: [RouterOutlet, UserProfileComponent, ChildComponent, TestcomComponent, ProjectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   viewProviders: [{provide:AnimalService, useClass: AnimalService}],
