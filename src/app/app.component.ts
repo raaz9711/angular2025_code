dd0fbdcafef01ddcb72524f4d83633641e61b215
raj kumar <raaz.kumar015@gmail.com>
2025-07-14 22:27:26 +0530

    creating a basic angular branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserProfileComponent } from "./user-profile/user-profile.component";
-import { AnimalService } from './animal.service';
-import { ChildComponent } from "./child/child.component";
-import { TestcomComponent } from "./signaldoc/testcom/testcom.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, UserProfileComponent, ChildComponent, TestcomComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  viewProviders: [{provide:AnimalService, useClass: AnimalService}],
 })
 export class AppComponent {
   title = 'code2025';
 
-  animal = inject(AnimalService);
 }
--- src/app/logger.service.ts
+++
@@ -1,5 +0,0 @@
-export class Logger {
-  log(msg: unknown) { console.log(msg); }
-  error(msg: unknown) { console.error(msg); }
-  warn(msg: unknown) { console.warn(msg); }
-}
